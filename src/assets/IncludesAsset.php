a147fb594a3b68e3232e9c71f52c65d4c43e0fac
mix8872 <mix8872@yandex.ru>
2019-12-24 18:33:14 +0300

    Fix asset

--- src/assets/IncludesAsset.php
+++ src/assets/IncludesAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class IncludesAsset extends AssetBundle
 {
-    public $sourcePath = '@common/modules/includes/assets';
+    public $sourcePath = '@vendor/mix8872/yii2-included-areas/src/assets';
 
     public $css = [
         'includes.css'
